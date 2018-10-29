8159b820174fd63cb3da84df6fe281de077e699f
Samuel Kebadu <famemansonic@gmail.com>
2018-10-29 16:01:39 -0400

    working on HW3

---
+++ Hw3.c
@@ -0,0 +1,24 @@
+//Author: Samuel Kebadu
+//G01053440
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char *getDateAndTime();
+int getInteger();
+
+int main(){
+	printf("Hello, World!\n");
+	printf("number is %d \n",getInteger());
+}
+
+int getInteger(){
+	int i;
+	char buffer[7];
+	printf("Enter an Integer( 1 - 1000000 ) or type x to exit: ");
+	fgets(buffer,8,stdin);
+	i = atoi(buffer);
+	return i;
+}
